b38ad24ec8cf764eb7a77c19b1ea0644cb08b43a
Tim <diamitse@gmail.com>
2021-05-02 23:30:45 +0600

    modified gh-pages support

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Header from "./1.header/Header";
 import Main from "./2.main/Main";
 import Footer from "./3.footer/Footer";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 const App = () => {
   const [activeAmount, setActiveAmount] = useState(0);
